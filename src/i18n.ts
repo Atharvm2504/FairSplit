52bf469f746a12b231857e4ded05a1715b874b4d
Atharva Rajendra Joshi <atharva.22311496@viit.ac.in>
2025-09-06 22:49:01 +0530

    Added expense group model & schema

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,33 +1,71 @@
+// import deepmerge from 'deepmerge'
+// import { getRequestConfig } from 'next-intl/server'
+// import { getUserLocale } from './lib/locale'
+
+// export const localeLabels = {
+//   'en-US': 'English',
+//   fi: 'Suomi',
+//   'fr-FR': 'Français',
+//   es: 'Español',
+//   'de-DE': 'Deutsch',
+//   'zh-CN': '简体中文',
+//   'zh-TW': '正體中文',
+//   'ja-JP': '日本語',
+//   'pl-PL': 'Polski',
+//   'ru-RU': 'Русский',
+//   'it-IT': 'Italiano',
+//   'ua-UA': 'Українська',
+//   ro: 'Română',
+//   'tr-TR': 'Türkçe',
+//   'pt-BR': 'Português Brasileiro',
+//   'nl-NL': 'Nederlands',
+//   ca: 'Català',
+//   'cs-CZ': 'Česky',
+// } as const
+
+// export const locales: (keyof typeof localeLabels)[] = Object.keys(
+//   localeLabels,
+// ) as any
+// export type Locale = keyof typeof localeLabels
+// export type Locales = ReadonlyArray<Locale>
+// export const defaultLocale: Locale = 'en-US'
+
+// export default getRequestConfig(async () => {
+//   const locale = await getUserLocale()
+//   const localeMessages = (await import(`../messages/${locale}.json`)).default
+
+//   let messages: any
+//   if (locale === defaultLocale) {
+//     messages = localeMessages
+//   } else {
+//     messages = deepmerge(
+//       (await import(`../messages/${defaultLocale}.json`)).default,
+//       localeMessages,
+//     ) as any
+//   }
+
+//   return {
+//     locale,
+//     messages,
+//   }
+// })
 import deepmerge from 'deepmerge'
 import { getRequestConfig } from 'next-intl/server'
 import { getUserLocale } from './lib/locale'
 
 export const localeLabels = {
   'en-US': 'English',
-  fi: 'Suomi',
-  'fr-FR': 'Français',
-  es: 'Español',
-  'de-DE': 'Deutsch',
-  'zh-CN': '简体中文',
-  'zh-TW': '正體中文',
-  'ja-JP': '日本語',
-  'pl-PL': 'Polski',
-  'ru-RU': 'Русский',
-  'it-IT': 'Italiano',
-  'ua-UA': 'Українська',
-  ro: 'Română',
-  'tr-TR': 'Türkçe',
-  'pt-BR': 'Português Brasileiro',
-  'nl-NL': 'Nederlands',
-  ca: 'Català',
-  'cs-CZ': 'Česky',
+  'mr-IN': 'मराठी',
+  'hi-IN': 'हिन्दी',
 } as const
 
 export const locales: (keyof typeof localeLabels)[] = Object.keys(
   localeLabels,
 ) as any
+
 export type Locale = keyof typeof localeLabels
 export type Locales = ReadonlyArray<Locale>
+
 export const defaultLocale: Locale = 'en-US'
 
 export default getRequestConfig(async () => {
--- src/lib/locale.ts
+++ src/lib/locale.ts
@@ -1,3 +1,46 @@
+// 'use server'
+
+// import { Locale, Locales, defaultLocale, locales } from '@/i18n'
+// import { match } from '@formatjs/intl-localematcher'
+// import Negotiator from 'negotiator'
+// import { cookies, headers } from 'next/headers'
+
+// const COOKIE_NAME = 'NEXT_LOCALE'
+
+// function getAcceptLanguageLocale(requestHeaders: Headers, locales: Locales) {
+//   let locale
+//   const languages = new Negotiator({
+//     headers: {
+//       'accept-language': requestHeaders.get('accept-language') || undefined,
+//     },
+//   }).languages()
+//   try {
+//     locale = match(languages, locales, defaultLocale)
+//   } catch (e) {
+//     // invalid language - fallback to default
+//     locale = defaultLocale
+//   }
+//   return locale
+// }
+
+// export async function getUserLocale() {
+//   let locale
+
+//   // Prio 1: use existing cookie
+//   locale = cookies().get(COOKIE_NAME)?.value
+
+//   // Prio 2: use `accept-language` header
+//   // Prio 3: use default locale
+//   if (!locale) {
+//     locale = getAcceptLanguageLocale(headers(), locales)
+//   }
+
+//   return locale
+// }
+
+// export async function setUserLocale(locale: Locale) {
+//   cookies().set(COOKIE_NAME, locale)
+// }
 'use server'
 
 import { Locale, Locales, defaultLocale, locales } from '@/i18n'
@@ -14,28 +57,34 @@ function getAcceptLanguageLocale(requestHeaders: Headers, locales: Locales) {
       'accept-language': requestHeaders.get('accept-language') || undefined,
     },
   }).languages()
+
   try {
     locale = match(languages, locales, defaultLocale)
   } catch (e) {
-    // invalid language - fallback to default
+    // fallback if no match
     locale = defaultLocale
   }
+
   return locale
 }
 
-export async function getUserLocale() {
-  let locale
+export async function getUserLocale(): Promise<Locale> {
+  let locale: string | undefined
 
   // Prio 1: use existing cookie
   locale = cookies().get(COOKIE_NAME)?.value
 
-  // Prio 2: use `accept-language` header
-  // Prio 3: use default locale
+  // Prio 2: use accept-language header
   if (!locale) {
     locale = getAcceptLanguageLocale(headers(), locales)
   }
 
-  return locale
+  // Ensure locale is one of the supported ones
+  if (!locales.includes(locale as Locale)) {
+    locale = defaultLocale
+  }
+
+  return locale as Locale
 }
 
 export async function setUserLocale(locale: Locale) {
